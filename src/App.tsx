fed6cd5c93c06c48dc077f89ae26fe8ec55a6e25
t.takahashi <takahashi.developer@gmail.com>
2020-10-03 21:54:16 +0900

    add sample list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 
-const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: 'bold' },
-});
+type ListItem = {
+  id: string;
+  title: string;
+};
+
+const DATA: ListItem[] = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item: React.FC<{ title: string }> = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>・ {title}</Text>
+  </View>
+);
 
 const App: React.FC = () => {
+  const renderItem: React.FC<{ item: ListItem }> = ({ item }) => (
+    <Item title={item.title} />
+  );
+
   return (
-    <View style={styles.box}>
-      <Text style={styles.text}>hello, world</Text>
+    <View>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 5,
+    marginVertical: 2,
+    marginHorizontal: 8,
+  },
+  title: {
+    fontSize: 24,
+  },
+});
 
 export default App;
